52aa5d36780e8af37afff9ff2b2ba1ad6eb37353
Tonia Del Priore <tonjagod@gmail.com>
2023-10-26 17:06:54 -0400

    add project links, no css:

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ function App() {
   return (
     <>
       <h1 className="name">Tonia Del Priore</h1>
+      <div> 
+        <a href="https://toninjaa.github.io/weather-app/" target="_blank">Weather Widget</a>
+      </div> 
+      <div>
+        <a href="https://toninjaa.github.io/palindrome-site/" target="_blank">Palindrome Detector</a>
+      </div>
+      <div>
+        <a href="https://rubygems.org/profiles/toninjaa" target="_blank">Ruby Gems</a>
+      </div>
     </>
   );
 }
