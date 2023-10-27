d1bb491883984b0a9657c7da6801aa56c63f331d
Tonia Del Priore <tonjagod@gmail.com>
2023-10-27 13:34:09 -0400

    add some motion

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ function App() {
   return (
     <div className="container">
       <h1 className="name">Tonia Del Priore</h1>
-      <div className="stage">
-        <a className="link-item" href="https://toninjaa.github.io/weather-app/" target="_blank">
+      <div className="stage1">
+        <a className="link-bubble" href="https://toninjaa.github.io/weather-app/" target="_blank">
           <span className="shadow"></span>
           Weather Widget
         </a>
       </div>
 
-      <div className="stage">
-        <a className="link-item" href="https://toninjaa.github.io/palindrome-site/" target="_blank">
+      <div className="stage2">
+        <a className="link-bubble" href="https://toninjaa.github.io/palindrome-site/" target="_blank">
           <span className="shadow"></span>
           Palindrome Detector
         </a>
       </div>
 
-      <div className="stage">
-        <a className="link-item" href="https://rubygems.org/profiles/toninjaa" target="_blank">
+      <div className="stage1">
+        <a className="link-bubble2" href="https://rubygems.org/profiles/toninjaa" target="_blank">
         <span className="shadow"></span>
           Ruby Gems
         </a>
