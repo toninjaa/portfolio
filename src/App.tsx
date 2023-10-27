63b93a02b97145a53042a35d1d183d6dc3e511e5
Tonia Del Priore <tonjagod@gmail.com>
2023-10-27 10:38:05 -0400

    add some css to links

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <h1 className="name">Tonia Del Priore</h1>
-      <div> 
-        <a href="https://toninjaa.github.io/weather-app/" target="_blank">Weather Widget</a>
-      </div> 
-      <div>
-        <a href="https://toninjaa.github.io/palindrome-site/" target="_blank">Palindrome Detector</a>
-      </div>
-      <div>
-        <a href="https://rubygems.org/profiles/toninjaa" target="_blank">Ruby Gems</a>
-      </div>
-    </>
+      <a className="link-item" href="https://toninjaa.github.io/weather-app/" target="_blank">Weather Widget</a>
+      <a className="link-item" href="https://toninjaa.github.io/palindrome-site/" target="_blank">Palindrome Detector</a>
+      <a className="link-item" href="https://rubygems.org/profiles/toninjaa" target="_blank">Ruby Gems</a>
+    </div>
   );
 }
 
