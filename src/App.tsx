295198bca44f603cf1e38255a58b691cee6459d3
Tonia Del Priore <tonjagod@gmail.com>
2023-11-09 14:36:40 -0500

    take away bubble anim and fix a bunch of movement, add colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,28 @@ function App() {
   return (
     <div className="container">
       <h1 className="name">Tonia Del Priore</h1>
-      <div className="stage1">
-        <a className="link-bubble" href="https://toninjaa.github.io/weather-app/" target="_blank">
-          <span className="shadow"></span>
-          Weather Widget
-        </a>
+      <div className="weather-container">
+        <div className="weather-bubble">
+          <a href="https://toninjaa.github.io/weather-app/" target="_blank">
+            Weather Widget
+          </a>
+        </div>
       </div>
 
-      <div className="stage2">
-        <a className="link-bubble" href="https://toninjaa.github.io/palindrome-site/" target="_blank">
-          <span className="shadow"></span>
-          Palindrome Detector
-        </a>
+      <div className="pal-container">
+        <div className="pal-bubble">
+          <a  href="https://toninjaa.github.io/palindrome-site/" target="_blank">
+            Palindrome Detector
+          </a>
+        </div>
       </div>
 
-      <div className="stage1">
-        <a className="link-bubble2" href="https://rubygems.org/profiles/toninjaa" target="_blank">
-        <span className="shadow"></span>
-          Ruby Gems
-        </a>
+      <div className="ruby-container">
+        <div className="ruby-bubble">
+          <a href="https://rubygems.org/profiles/toninjaa" target="_blank">
+            Ruby Gems
+          </a>
+        </div>
       </div>
     </div>
   );
