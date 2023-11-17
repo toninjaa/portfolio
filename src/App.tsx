44acb12adc13e394eb345f9376289a11e15b8951
Tonia Del Priore <tonjagod@gmail.com>
2023-11-17 15:13:05 -0500

    add project box and bubble shadow

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,30 @@ function App() {
   return (
     <>
       <h1 className="name">Tonia Del Priore</h1>
-      <div className="weather-container">
-        <div className="weather-bubble">
-          <a href="https://toninjaa.github.io/weather-app/" target="_blank">
-            Weather Widget
-          </a>
+      <div className="project-div">
+        <h3 className="project-header">Projects</h3>
+        <div className="weather-container">  
+          <div className="weather-bubble">
+            <a href="https://toninjaa.github.io/weather-app/" target="_blank">
+              Weather Widget
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div className="pal-container">
-        <div className="pal-bubble">
-          <a  href="https://toninjaa.github.io/palindrome-site/" target="_blank">
-            Palindrome Detector
-          </a>
+        <div className="pal-container">
+          <div className="pal-bubble">
+            <a  href="https://toninjaa.github.io/palindrome-site/" target="_blank">
+              Palindrome Detector
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div className="ruby-container">
-        <div className="ruby-bubble">
-          <a href="https://rubygems.org/profiles/toninjaa" target="_blank">
-            Ruby Gems
-          </a>
+        <div className="ruby-container">
+          <div className="ruby-bubble">
+            <a href="https://rubygems.org/profiles/toninjaa" target="_blank">
+              Ruby Gems
+            </a>
+          </div>
         </div>
       </div>
     </>
